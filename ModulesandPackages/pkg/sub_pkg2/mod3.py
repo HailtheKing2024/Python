98c9547fd62e5320e25e83401bc5ad0ae3dcfac7
Abhinav Bijish <bijishabhinav@gmail.com>
2026-02-14 16:25:13 -0800

    2-13 modules work

---
+++ ModulesandPackages/pkg/sub_pkg2/mod3.py
@@ -0,0 +1,5 @@
+def merge_data():
+    print('Merging data using merge_data()')
+
+class Message:
+    pass
---
+++ ModulesandPackages/pkg/sub_pkg2/mod4.py
@@ -0,0 +1,5 @@
+def send_mail():
+    print('Sending mail using send_mail()')
+
+class Winner:
+    pass
