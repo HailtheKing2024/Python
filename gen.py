dc1cc85e5f1efc3a71fc7e4256700af437ad3861
Abhinav Bijish <bijishabhinav@gmail.com>
2026-03-08 12:44:44 -0700

    a

--- gen.py
+++ gen.py
@@ -9,4 +9,4 @@ user = input('Enter number to start countdown:')
 numbers = countdown(int(user))
 
 for i in numbers:
-    print(i) # Prints numbers 
+    print(i) # Prints numbers withhin the range of the user input and 0.
